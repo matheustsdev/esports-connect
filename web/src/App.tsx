e9190f115ca82560867e44eb60e74276b812812d
Matheus <matheusts.dev@gmail.com>
2022-09-20 16:55:16 -0300

    feat: style of landing page completed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
@@ -64,7 +66,16 @@ function App() {
       </div>
 
       <div className="pt-1 mt-8  bg-esports-gradient self-stretch rounded-lg overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 ">dfdfdfdf</div>
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-500">Publique um anúncio para encontrar novos players</span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 text-white rounded flex items-center gap-3 hover:bg-violet-600">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
